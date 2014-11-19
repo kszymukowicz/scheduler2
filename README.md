f8e9cad6094ec56db9b6a15ad58663e2b462d081
Krystian Szymukowicz <k.szymukowicz@sourcebroker.net>
2014-11-19 22:23:58 +0100

    Readme.

---
+++ README.md
@@ -0,0 +1,4 @@
+scheduler2
+==========
+
+Simple TYPO3 Scheduler task example for functions getAdditionalInformation() and getProgress().
--- ext_localconf.php
+++ ext_localconf.php
@@ -2,7 +2,6 @@
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['V\\Scheduler2\\Task\\SampleTask'] = array(
 		'extension' => $_EXTKEY,
-		'title' => 'The simplest task ever',
-		'description' => 'Task description',
-		'additionalFields' => 'V\\Scheduler2\\Task\\SampleTaskAdditionalFieldProvider'
+		'title' => 'The simplest task ever - getAdditionalInformation() and getProgress()',
+		'description' => 'Scheduler Test - The simplest extension with task - getAdditionalInformation() and getProgress() example',
 );
